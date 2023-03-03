2d00f9254526d85be59faa3ccc8e99b73dd9c210
Anas Bin Azim <anask3994@gmail.com>
2023-03-03 23:50:03 +0500

    anas

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:whatsapp_clone/homepage.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,12 +11,13 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 3),
+        const Duration(seconds: 3),
         () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => HomePage())));
+            context, MaterialPageRoute(builder: (context) => const HomePage())));
   }
 
   @override
@@ -32,13 +31,13 @@ class _SplashScreenState extends State<SplashScreen> {
             Container(
               height: 60,
               width: 60,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage('assets/images/WhatsappLogo.png'),
                     fit: BoxFit.fill),
               ),
             ),
-            Text(
+            const Text(
               "WhatsApp",
               style: TextStyle(
                   fontFamily: 'Roboto',
